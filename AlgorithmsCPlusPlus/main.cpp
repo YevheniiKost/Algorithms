7199b10e091a9f6e4a5509dc5a1a850f9f210217
YevheniiKost <atom903@gmail.com>
2023-10-11 20:35:06 +0300

    Add c++ project with bubble sort

---
+++ AlgorithmsCPlusPlus/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "SortingAlgorithms.h"
+
+void printArray(int arr[], int size){
+    for(int i = 0; i < size; i++){
+        std::cout << arr[i] << " "<< std::endl;
+    }
+    std::cout << std::endl;
+}
+
+int main(){
+    int arr[] = {5, 4, 3, 2, 1};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    std::cout << "Before sorting: " << std::endl;
+    printArray(arr, size);
+    bubbleSort(arr, size);
+    std::cout << "After sorting: " << std::endl;
+    printArray(arr, size);
+    return 0;
+}
